d20aeebe0d4b6e155684a37363bc95f9eb03b05f
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2024-02-14 14:48:26 -0300

    Exercício de programação orientada a objetos em kotlin.

--- src/collections/Set02.kt
+++ src/collections/Set02.kt
@@ -1,21 +1,23 @@
 package collections
 
+import java.util.Comparator
 import java.util.Objects
 
 fun main() {
-    val dogs = setOf(
-        Dog("D1"),
-        Dog("D2"),
+    val dogs = sortedSetOf(
+        Comparator.comparing{dog: Dog -> dog.name },
         Dog("D3"),
+        Dog("D2"),
+        Dog("D5"),
         Dog("D4"),
-        Dog("D4"),
+        Dog("D1"),
     )
     println(dogs)
 
 
 }
 
-class Dog(private val name: String) {
+class Dog( val name: String) {
     override fun toString() = "Dog(name=$name, hashCode=${hashCode()})"
 
     override fun hashCode(): Int {
--- src/convertColletions/Ex02.kt
+++ src/convertColletions/Ex02.kt
@@ -1,3 +1,11 @@
 package convertColletions
 
+import java.util.Comparator
+
+fun main() {
+    val s = sortedSetOf(Comparator.naturalOrder(),1, 3, 8, 6, 4, 5, 7, 9, 2)
+    println(s)
+}
+
+
 
